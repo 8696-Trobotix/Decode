9e64331c4f442e1a47f64f9d65fb476527a903cb
ハイドラ <thecomputer314@gmail.com>
2025-11-14 16:13:14 -0600

    Remove encoder from Flywheel
    - Tada

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
@@ -12,26 +12,14 @@ import org.firstinspires.ftc.lib.wpilib.command.SubsystemBase;
 import org.firstinspires.ftc.lib.wpilib.math.system.plant.DCMotor;
 
 public class Flywheel extends SubsystemBase {
-  private final ModeledMotor motor =
-      new ModeledMotor(
-          new Motor("Motor5"),
-          new Encoder("Motor5", Encoder.CountsPerRevolution.GOBILDA_6000RPM),
-          DCMotor.GOBILDA_5203_6000RPM(1),
-          9,
-          5);
-
-    @Override
-    public void periodic() {
-        Telemetry.addDashboardData("Flywheel/Velocity RPM", motor.getEncoder().getVelocity() * 60);
-    }
+  private final Motor motor = new Motor("Motor4");
 
     public Command spinUp() {
     return run(
         () -> {
           double targetRPM = 5000;
           var feedforward = targetRPM * (12.0 / 6000);
-          var feedback = (12.0 / 100) * (targetRPM / 60.0 - motor.getEncoder().getVelocity());
-          motor.setVoltage(feedforward + feedback);
+          motor.setVoltage(feedforward);
         });
   }
 }
